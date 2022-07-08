4a22236e6a0e6bc0437efe2ca3336a0cf7783df2
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-07-08 17:46:56 -0300

    Isso são os controles para Android

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -185,7 +185,9 @@ class StoryMenuState extends MusicBeatState
 		updateText();
 
 		trace("Line 165");
-
+                #if android
+addVirtualPad(FULL, A_B);
+#end
 		super.create();
 	}
 
@@ -244,12 +246,12 @@ class StoryMenuState extends MusicBeatState
 					}
 				}
 
-				if (FlxG.keys.justPressed.UP)
+				if (controls.UP_P)
 				{
 					changeWeek(-1);
 				}
 
-				if (FlxG.keys.justPressed.DOWN)
+				if (controls.DOWN_P)
 				{
 					changeWeek(1);
 				}
