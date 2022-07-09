58833d1a02381af4ce18f75853d8b9ddd5cd6265
Josenene8 <100051827+Josenene8@users.noreply.github.com>
2022-07-08 21:34:03 -0300

    Update FreeplayState.hx

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -144,7 +144,11 @@ class FreeplayState extends MusicBeatState
 
 			trace(md);
 		 */
-
+		
+                #if android
+		addVirtualPad(FULL, A_B);
+		#end
+			
 		super.create();
 	}
 
@@ -220,9 +224,9 @@ class FreeplayState extends MusicBeatState
 			changeSelection(1);
 		}
 
-		if (FlxG.keys.justPressed.LEFT)
+		if (controls.LEFT_P)
 			changeDiff(-1);
-		if (FlxG.keys.justPressed.RIGHT)
+		if (controls.RIGHT_P)
 			changeDiff(1);
 
 		if (controls.BACK)
